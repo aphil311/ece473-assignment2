c3d178b068748a994336f300badbeeb133f5ff89
Aidan Phillips <41242437+aphil311@users.noreply.github.com>
2023-03-02 01:16:57 -0500

    wrote getLatestPrice

--- PriceFeed.sol
+++ PriceFeed.sol
@@ -6,4 +6,26 @@ import "./interfaces/IPriceFeed.sol";
 
 contract PriceFeed is IPriceFeed {
     /* TODO: implement your functions here */
+    AggregatorV3Interface internal priceFeed;
+
+    constructor() {
+        priceFeed = AggregatorV3Interface(
+            // Tesla's stock price
+            0x982B232303af1EFfB49939b81AD6866B2E4eeD0B
+        );
+    }
+
+    function getLatestPrice() external view returns (
+        int price,
+        uint lastUpdatedTime
+    ){
+        (
+            uint80 roundID,
+            int price,
+            uint startedAt,
+            uint timeStamp,
+            uint80 answeredInRound
+        ) = priceFeed.latestRoundData();
+        return (price, timeStamp);
+    }
 }
